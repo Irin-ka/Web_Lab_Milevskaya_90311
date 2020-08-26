51446a88c4b975318324bc096ba7d2f400f6654a
Ирина Ирина <irin-ka@inbox.ru>
2020-08-26 15:14:52 +0300

    Лабораторная работа 3

--- Web_Lab_Milevskaya_90311/Components/MenuViewComponent.cs
+++ Web_Lab_Milevskaya_90311/Components/MenuViewComponent.cs
@@ -18,25 +18,25 @@ namespace Web_Lab_Milevskaya_90311.Components
 
         public IViewComponentResult Invoke()
         {
-            //Получение значений сегментов маршрута 
-            //var controller = ViewContext.RouteData.Values["controller"];
-            //var page = ViewContext.RouteData.Values["page"];
-            //var area = ViewContext.RouteData.Values["area"];
+           // Получение значений сегментов маршрута
+            var controller = ViewContext.RouteData.Values["controller"];
+            var page = ViewContext.RouteData.Values["page"];
+            var area = ViewContext.RouteData.Values["area"];
 
-            //foreach (var item in _menuItems)
-            //{
-            //    // Название контроллера совпадает? 
-            //    var _matchController = controller?.Equals(item.Controller)
-            //   ?? false;
-            //     // Название зоны совпадает? 
-            //    var _matchArea = area?.Equals(item.Area) ?? false;
-            //    // Если есть совпадение, то сделать элемент меню активным  
-            //    // (применить соответствующий класс CSS) 
-            //    if (_matchController || _matchArea)
-            //    {
-            //        item.Active = "active";
-            //    }
-            //}
+            foreach (var item in _menuItems)
+            {
+                // Название контроллера совпадает? 
+                var _matchController = controller?.Equals(item.Controller)
+               ?? false;
+                // Название зоны совпадает? 
+                var _matchArea = area?.Equals(item.Area) ?? false;
+                // Если есть совпадение, то сделать элемент меню активным  
+                // (применить соответствующий класс CSS) 
+                if (_matchController || _matchArea)
+                {
+                    item.Active = "active";
+                }
+            }
             return View(_menuItems);
         }
 
